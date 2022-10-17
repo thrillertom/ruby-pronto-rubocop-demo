9a4a9217439b012b90d9d784fbbe49445e65a7fc
xiaolong <thrillertom@gmail.com>
2022-10-17 15:34:48 +0800

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,4 @@ class Comment < ApplicationRecord
   attr :author, :text, :status
 
   enum comment_status: { draft: 0, resolved: 1 }
-
-  def resolve
-    status = :comment_status[:resolved]
-    save!
-  end
 end
